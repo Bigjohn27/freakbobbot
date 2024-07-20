a4933db79c0397d652fb38ecefa1739a30b40b04
Bigjohn27 <55621893+Bigjohn27@users.noreply.github.com>
2024-07-20 06:34:49 +0100

    Update python.py

--- python.py
+++ python.py
@@ -3,10 +3,34 @@ from discord.ext import commands
 import os
 from keep_alive import keep_alive  # Import the keep_alive function
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
 bz = int(os.getenv('b'))
+
+ge = int(os.getenv('g'))
+wi = int(os.getenv('w'))
+di = int(os.getenv('d'))
+ki = int(os.getenv('k'))
+
+People = [ge, wi, di, ki, bz]
+
+diList = [Dino1.ogg, Dino2.ogg]
+geList = [George1.ogg, George2.ogg]
+kiList = [Kieran1.ogg, Kieran2.ogg]
+wiList = [Willy1.ogg, Willy2.ogg]
+
+def findAudio(thePerson):
+    if thePerson == ge or bz:
+        return random.choice(geList)
+    if thePerson == di:
+        return random.choice(diList)
+    if thePerson == ki:
+        return random.choice(kiList)
+    if thePerson == wi:
+        return random.choice(wiList)
+    
 
 # Define intents
 intents = discord.Intents.default()
@@ -31,6 +55,9 @@ async def on_message(message):
     # Check if the message contains the word 'freakbob' or the freakbob emoji (case insensitive)
     if 'freakbob' in message.content.lower():
         await message.channel.send(file=discord.File('bob.jpg'))
+        if message.author.id in People:
+            person = message.author.id
+            await message.channel.send(file=discord.File(findAudio(person), filename=Pick up the phone.ogg))                
     if ':emoji_1:' in message.content:
         await message.channel.send(file=discord.File('bob.jpg'))
     if 'daaaa' in message.content.lower():
